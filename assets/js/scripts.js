f04b06650e7fed00eb99536aa65998d4b4a6fa99
JkKU <60291632+Jenrikku@users.noreply.github.com>
2021-09-06 10:40:49 +0200

    Updated the page to use Github (test)

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,10 +1,7 @@
-document.getElementById('content').classList.remove('animatedHide');
-document.getElementById('projectFrame').classList.remove('animatedShow');
+document.getElementById("content").classList.remove("animatedHide");
+document.getElementById("projectFrame").classList.remove("animatedShow");
 
-document.querySelector('#projectsButton').addEventListener('click', () => {
-	document.getElementById('content').classList.add('animatedHide');
-	document.getElementById('projectFrame').style.display = 'block';
-	setTimeout(function () {
-		document.getElementById('projectFrame').classList.add('animatedShow');
-	}, 1300);
+document.querySelector("#projectsButton").addEventListener("click", () => {
+  document.getElementById("content").classList.add("animatedHide");
+  document.getElementById("projectFrame").classList.add("animatedShow");
 });
